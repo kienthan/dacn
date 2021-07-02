1e0a38542c40c1442df278af8097e0cc94e67fe6
nguyenductung <nguyenductung9460@gmail.com>
2021-07-02 14:54:54 +0700

    update Footer !!!

--- doanchuyennganh/src/frontend/footer.js
+++ doanchuyennganh/src/frontend/footer.js
@@ -1,99 +1,147 @@
 import React from 'react'
-
+import "./footer.css";
 export default function Footer(){
     return(
-        <footer className="page-footer font-small mdb-color lighten-3 pt-4">
+        <div>
+            <footer className="page-footer font-small unique-color-dark mt-2">
 
-        <div class="container text-center text-md-left">
+            <div style={{backgroundColor: '#6351ce'}}>
+                <div className="container">
 
-            <div class="row">
+                <div className="row py-4 d-flex align-items-center">
+                    <div className="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+                    <h6 className="mb-0" style={{color:'white'}}>
+                        Get connected with us on social networks!</h6>
+                    </div>
+                    <div className="col-md-6 col-lg-7 text-center text-md-right">
+                    
+                    <a className="ml-3" href="#">
+                        <img src="./img/fb.png" alt="Facebook"
+                        width='30px' height='30px'/>
+                    </a>
+    
+                    <a className="ml-3" href="#">
+                        <img src="./img/twitter.png" alt="Twitter"
+                        width='30px' height='30px'/>
+                    </a>
+            
+                    <a className="ml-3" href="#">
+                        <img src="./img/google.png" alt="Google"
+                        width='30px' height='30px'/>
+                    </a>
+        
+                    <a className="ml-3" href="#">
+                        <img src="./img/linkedin.png" alt="Linkedin"
+                        width='30px' height='30px'/>
+                    </a>
+            
+                    <a className="ml-3" href="#">
+                        <img src="./img/instagram.png" alt="Instagram"
+                        width='30px' height='30px'/>
+                    </a>
 
-            <div class="col-md-4 col-lg-3 mr-auto my-md-4 my-0 mt-4 mb-1">
+                    </div>
+        
 
-                <h5 class="font-weight-bold text-uppercase mb-4">Footer Content</h5>
-                <p>Here you can use rows and columns to organize your footer content.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit amet numquam iure provident voluptate
-                esse
-                quasi, veritatis totam voluptas nostrum.</p>
+                </div>
+        
 
+                </div>
             </div>
 
-            <hr class="clearfix w-100 d-md-none" />
 
-            <div class="col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1">
-                <h5 class="font-weight-bold text-uppercase mb-4">About</h5>
+            <div className="container-fluid text-center text-md-left"
+             style={{backgroundColor:'#39364D', color:'white'}}>
 
-                <ul class="list-unstyled">
-                <li>
+                <div className="row">
+
+    
+                <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4 mt-3">
+
+    
+                    <h6 className="text-uppercase font-weight-bold">Company name</h6>
+                    <hr />
+
+                    <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+                    consectetur
+                    adipisicing elit.</p>
+
+                </div>
+    
+
+    
+                <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 mt-3">
+
+
+                    <h6 className="text-uppercase font-weight-bold">Products</h6>
+                    <hr/>
                     <p>
-                    <a href="#!">PROJECTS</a>
+                    <a href="#!">MDBootstrap</a>
                     </p>
-                </li>
-                <li>
                     <p>
-                    <a href="#!">ABOUT US</a>
+                    <a href="#!">MDWordPress</a>
                     </p>
-                </li>
-                <li>
                     <p>
-                    <a href="#!">BLOG</a>
+                    <a href="#!">BrandFlow</a>
                     </p>
-                </li>
-                <li>
                     <p>
-                    <a href="#!">AWARDS</a>
+                    <a href="#!">Bootstrap Angular</a>
                     </p>
-                </li>
-                </ul>
+
+                </div>
+            
+
+            
+                <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4 mt-3">
+
+                
+                    <h6 className="text-uppercase font-weight-bold">Useful links</h6>
+                    <hr/>
+                    <p>
+                    <a href="#!">Your Account</a>
+                    </p>
+                    <p>
+                    <a href="#!">Become an Affiliate</a>
+                    </p>
+                    <p>
+                    <a href="#!">Shipping Rates</a>
+                    </p>
+                    <p>
+                    <a href="#!">Help</a>
+                    </p>
+
+                </div>
+            
+                <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 mt-3">
+
+                
+                    <h6 className="text-uppercase font-weight-bold">Contact</h6>
+                    <hr/>
+                    <p>
+                    <i className="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+                    <p>
+                    <i className="fas fa-envelope mr-3"></i> info@example.com</p>
+                    <p>
+                    <i className="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+                    <p>
+                    <i className="fas fa-print mr-3"></i> + 01 234 567 89</p>
+
+                </div>
+            
 
             </div>
-            <hr class="clearfix w-100 d-md-none" />
-            <div class="col-md-4 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">
-                <h5 class="font-weight-bold text-uppercase mb-4">Address</h5>
-
-                <ul class="list-unstyled">
-                <li>
-                    <p>
-                    <i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-                </li>
-                <li>
-                    <p>
-                    <i class="fas fa-envelope mr-3"></i> info@example.com</p>
-                </li>
-                <li>
-                    <p>
-                    <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-                </li>
-                <li>
-                    <p>
-                    <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
-                </li>
-                </ul>
+        
 
             </div>
-            <hr class="clearfix w-100 d-md-none" />
-            <div class="col-md-2 col-lg-3 text-center mx-auto my-4">
+                <div className="footer-copyright text-center py-3" 
+                    style={{backgroundColor:'#080808', color:'#989898'}}>
+                    © 2021 Copyright:
+                    <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+                </div>
+            
 
-                <h5 class="font-weight-bold text-uppercase mb-4">Follow Us</h5>
-
-                <a type="button" class="btn-floating btn-fb" href="#">
-                <i class="fab fa-facebook"></i>
-                </a>
-                <a type="button" class="btn-floating btn-tw">
-                <i class="fab fa-twitter"></i>
-                </a>
-                <a type="button" class="btn-floating btn-gplus">
-                <i class="fab fa-google-plus-g"></i>
-                </a>
-                <a type="button" class="btn-floating btn-dribbble">
-                <i class="fab fa-dribbble"></i>
-                </a>
-
-            </div>
+            </footer>
         </div>
-        </div>
-        </footer>
-      
 
     )
 }
